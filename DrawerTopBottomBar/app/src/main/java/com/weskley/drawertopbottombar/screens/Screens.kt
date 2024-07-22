afaddae69d44b6b824477fd79a557c46ea381a69
weskley-moreira <weskleymdev@gmail.com>
2024-07-22 20:33:37 -0300

    Full Home Screen Example Compose

---
+++ DrawerTopBottomBar/app/src/main/java/com/weskley/drawertopbottombar/screens/Screens.kt
@@ -0,0 +1,10 @@
+package com.weskley.drawertopbottombar.screens
+
+sealed class Screens(val screen: String) {
+    data object Home: Screens("home")
+    data object Search: Screens("search")
+    data object Notification: Screens("notification")
+    data object Setting: Screens("setting")
+    data object Profile: Screens("profile")
+    data object Post: Screens("post")
+}
