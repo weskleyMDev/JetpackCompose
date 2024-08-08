6398a1c1e9b9e785aec09632d257f4f2898dd2f9
weskley-moreira <weskleymdev@gmail.com>
2024-08-08 19:16:18 -0300

    TimePickerDialog Updated

--- PushNotification/DateTimePicker/app/src/main/java/com/weskley/datetimepicker/MainActivity.kt
+++ PushNotification/DateTimePicker/app/src/main/java/com/weskley/datetimepicker/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +18,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,7 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -49,7 +53,7 @@ class MainActivity : ComponentActivity() {
             DateTimePickerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ShowPicker(
-                        modifier = Modifier.padding(innerPadding)
+                        innerPadding,
                     )
                 }
             }
@@ -60,7 +64,7 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShowPicker(
-    modifier: Modifier = Modifier
+    paddingValues: PaddingValues
 ) {
     val calendar = Calendar.getInstance()
     val state = rememberTimePickerState(
@@ -68,38 +72,50 @@ fun ShowPicker(
         initialHour = calendar.get(Calendar.HOUR_OF_DAY),
         initialMinute = calendar.get(Calendar.MINUTE)
     )
-    var showPicker by remember { mutableStateOf(false) }
-    var showTimer by remember { mutableStateOf(false) }
+    var showContent by remember { mutableStateOf(false) }
+    var changeContent by remember { mutableStateOf(false) }
+    val toggleIcon = if (changeContent) R.drawable.outline_nest_clock_farsight_analog_24 else R.drawable.outline_nest_clock_farsight_digital_24
     var hour by remember {
-        mutableStateOf(calendar.get(Calendar.HOUR_OF_DAY).toString())
+        mutableStateOf(calendar.get(Calendar.HOUR_OF_DAY).toString().padStart(2,'0'))
     }
     var minute by remember {
-        mutableStateOf(calendar.get(Calendar.MINUTE).toString())
+        mutableStateOf(calendar.get(Calendar.MINUTE).toString().padStart(2,'0'))
     }
     Column(
-        modifier = modifier
-            .fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = paddingValues.calculateTopPadding()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { showPicker = true }) {
+        Button(onClick = { showContent = true }) {
             Text(text = "Show")
         }
-        Text(text = "TIME= $hour : $minute")
+        Text(text = "TIME= $hour:$minute")
     }
-    if (showPicker) {
+    if (showContent) {
         MyTimePickerDialog(
-            onCancel = { showPicker = false },
+            onCancel = {
+                showContent = false
+                changeContent = false
+            },
             onConfirm = {
-                showPicker = false
                 calendar.set(Calendar.HOUR_OF_DAY, state.hour)
                 calendar.set(Calendar.MINUTE, state.minute)
-                hour = state.hour.toString()
-                minute = state.minute.toString()
+                hour = state.hour.toString().padStart(2,'0')
+                minute = state.minute.toString().padStart(2,'0')
+                showContent = false
+                changeContent = false
             },
-            change = { showTimer = !showTimer },
+            toggle = {
+                IconButton(onClick = {
+                    changeContent = !changeContent
+                }) {
+                    Icon(painter = painterResource(id = toggleIcon), contentDescription = null)
+                }
+            },
             content = {
-                if (!showTimer) TimePicker(state = state) else TimeInput(state = state)
+                if (!changeContent) TimePicker(state = state) else TimeInput(state = state)
             }
         )
     }
@@ -109,65 +125,53 @@ fun MyTimePickerDialog(
     title: String = "Select Time",
     onCancel: () -> Unit,
     onConfirm: () -> Unit,
-    change: () -> Unit = {},
+    toggle: @Composable () -> Unit,
     content: @Composable () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
-        Dialog(
-            onDismissRequest = onCancel,
-            properties = DialogProperties(usePlatformDefaultWidth = false)
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier = Modifier
+                .width(IntrinsicSize.Max)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                )
         ) {
-            Surface(
-                shape = MaterialTheme.shapes.extraLarge,
-                tonalElevation = 2.dp,
+            Column(
                 modifier = Modifier
-                    .width(IntrinsicSize.Max)
-                    .height(IntrinsicSize.Min)
-                    .background(
-                        shape = MaterialTheme.shapes.extraLarge,
-                        color = MaterialTheme.colorScheme.surface
-                    )
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(
+                Text(
+                    text = title,
                     modifier = Modifier
-                        .padding(20.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .fillMaxWidth()
+                        .padding(bottom = 24.dp),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
                 ) {
-                    Text(
-                        text = title,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 18.dp, top = 18.dp, bottom = 18.dp),
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                        fontWeight = FontWeight.Bold
-                    )
-                    content()
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(end = 12.dp),
-                        horizontalArrangement = Arrangement.End,
-                        verticalAlignment = Alignment.CenterVertically
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(
+                        onClick = onCancel
                     ) {
-                        TextButton(onClick = change) {
-                            Text(text = "Change")
-                        }
-                        TextButton(
-                            onClick = onCancel
-                        ) {
-                            Text(text = "Cancel")
-                        }
-                        TextButton(
-                            onClick = onConfirm
-                        ) {
-                            Text(text = "Confirm")
-                        }
+                        Text(text = "Cancel")
+                    }
+                    TextButton(
+                        onClick = onConfirm
+                    ) {
+                        Text(text = "OK")
                     }
                 }
             }
@@ -179,6 +183,6 @@ fun MyTimePickerDialog(
 @Composable
 fun GreetingPreview() {
     DateTimePickerTheme {
-        ShowPicker()
+
     }
 }
