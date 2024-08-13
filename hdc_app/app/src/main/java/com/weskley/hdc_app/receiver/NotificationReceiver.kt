a8c3df99296656a434f40d35b1c24b959d79d2b2
weskley-moreira <weskleymdev@gmail.com>
2024-08-13 18:42:39 -0300

    Trigger Notification with Image at Schedule Time(UPDATED)

--- hdc_app/app/src/main/java/com/weskley/hdc_app/receiver/NotificationReceiver.kt
+++ hdc_app/app/src/main/java/com/weskley/hdc_app/receiver/NotificationReceiver.kt
@@ -1,19 +1,9 @@
 package com.weskley.hdc_app.receiver
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.net.toUri
-import com.weskley.hdc_app.MainActivity
-import com.weskley.hdc_app.R
-import com.weskley.hdc_app.controller.MY_URI
 import com.weskley.hdc_app.service.imp.NotificationServiceImp
 import com.weskley.hdc_app.viewmodel.AlarmViewModel
 
@@ -28,36 +18,4 @@ class NotificationReceiver: BroadcastReceiver() {
             Log.d("ERROR:","${e.printStackTrace()}")
         }
     }
-}
-
-fun showNotification(context: Context?, title: String, message: String) {
-    val manager = context?.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-    val channelId = "channel_id"
-    val channelName = "channel_name"
-    val channelDescription = "channel_description"
-
-    val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
-    channel.description = channelDescription
-    manager.createNotificationChannel(channel)
-
-    val clickIntent = Intent(
-        Intent.ACTION_VIEW,
-        MY_URI.toUri(),
-        context,
-        MainActivity::class.java
-    ).let { intent ->
-        TaskStackBuilder.create(context).run {
-            addNextIntentWithParentStack(intent)
-            getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE)
-        }
-    }
-
-    val builder = NotificationCompat.Builder(context, channelId)
-        .setContentTitle(title)
-        .setContentText(message)
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentIntent(clickIntent)
-        .setAutoCancel(true)
-
-    manager.notify(0, builder.build())
 }
