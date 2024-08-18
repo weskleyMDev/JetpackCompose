15da2adee74f1ea06800305e16361e90196a504d
weskley-moreira <weskleymdev@gmail.com>
2024-08-18 09:37:19 -0300

    Full Room DB Complete (Updated)

--- RoomDB/app/src/main/java/com/weskley/roomdb/TodoDialog.kt
+++ RoomDB/app/src/main/java/com/weskley/roomdb/TodoDialog.kt
@@ -2,23 +2,92 @@ package com.weskley.roomdb
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Alarm
+import androidx.compose.material.icons.twotone.Keyboard
+import androidx.compose.material.icons.twotone.NotificationsActive
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoDialog(
     state: TodoState,
     viewModel: TodoViewModel = hiltViewModel()
 ) {
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+    val changePicker = remember {
+        mutableStateOf(false)
+    }
+    val calendar = Calendar.getInstance()
+    val timePickerState = rememberTimePickerState(
+        initialHour = calendar.get(Calendar.HOUR_OF_DAY),
+        initialMinute = calendar.get(Calendar.MINUTE),
+        is24Hour = true
+    )
+    val formatter = remember {
+        SimpleDateFormat("HH:mm", Locale.getDefault())
+    }
+    if (openDialog.value) {
+        AlertDialog(
+            title = { Text(text = "SELECIONE UM HORÁRIO")},
+            text = {
+                if (!changePicker.value)
+                    TimePicker(state = timePickerState)
+                else
+                    TimeInput(state = timePickerState)
+            },
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    calendar.set(Calendar.HOUR_OF_DAY, timePickerState.hour)
+                    calendar.set(Calendar.MINUTE, timePickerState.minute)
+                    viewModel.onEvent(TodoEvent.SetImage(formatter.format(calendar.time)))
+                    openDialog.value = false
+                }) {
+                    Text(text = "OK")
+                }
+            },
+            dismissButton = {
+                IconButton(onClick = {
+                    changePicker.value = !changePicker.value
+                }) {
+                    if (!changePicker.value)
+                        Icon(imageVector = Icons.TwoTone.Keyboard, contentDescription = null)
+                    else
+                        Icon(imageVector = Icons.TwoTone.Alarm, contentDescription = null)
+                }
+            },
+        )
+    }
     AlertDialog(
         title = {
-            Text(text = "Add Todo")
+            Text(text = "ALARME")
         },
         text = {
             Column(
@@ -26,32 +95,51 @@ fun TodoDialog(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 TextField(
+                    textStyle = MaterialTheme.typography.bodyLarge,
                     value = state.title,
                     onValueChange = {
                         viewModel.onEvent(TodoEvent.SetTitle(it))
                     },
                     placeholder = {
-                        Text(text = "Title")
+                        Text(text = "Título")
                     }
                 )
                 TextField(
+                    textStyle = MaterialTheme.typography.bodyLarge,
                     value = state.text,
                     onValueChange = {
                         viewModel.onEvent(TodoEvent.SetText(it))
                     },
                     placeholder = {
-                        Text(text = "Text")
+                        Text(text = "Texto")
                     }
                 )
-                TextField(
-                    value = state.image,
-                    onValueChange = {
-                        viewModel.onEvent(TodoEvent.SetImage(it))
-                    },
-                    placeholder = {
-                        Text(text = "Image")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextField(
+                        modifier = Modifier
+                            .weight(1f),
+                        textStyle = MaterialTheme.typography.bodyLarge,
+                        readOnly = true,
+                        value = state.image,
+                        placeholder = {
+                            Text(
+                                text = "Hora",
+                            )
+                        },
+                        onValueChange = {}
+                    )
+                    IconButton(onClick = {
+                        openDialog.value = true
+                    }) {
+                        Icon(
+                            modifier = Modifier.size(28.dp),
+                            imageVector = Icons.TwoTone.NotificationsActive,
+                            contentDescription = "Access Alarm"
+                        )
                     }
-                )
+                }
             }
         },
         onDismissRequest = {
@@ -61,7 +149,14 @@ fun TodoDialog(
             TextButton(onClick = {
                 viewModel.onEvent(TodoEvent.SaveTodo)
             }) {
-                Text(text = "Save")
+                Text(text = "SALVAR")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = {
+                viewModel.onEvent(TodoEvent.HideDialog)
+            }) {
+                Text(text = "CANCELAR")
             }
         }
     )
--- RoomDB/app/src/main/java/com/weskley/roomdb/TodoScreen.kt
+++ RoomDB/app/src/main/java/com/weskley/roomdb/TodoScreen.kt
@@ -1,23 +1,33 @@
 package com.weskley.roomdb
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.twotone.Delete
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -40,7 +50,7 @@ fun TodoScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = paddingValues.calculateTopPadding()),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             if (state.isDialogOpen) {
                 item {
@@ -48,16 +58,45 @@ fun TodoScreen(
                 }
             }
             items(state.todoList) { todo ->
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(text = todo.title)
-                        Text(text = todo.text)
-                        Text(text = todo.image)
-                    }
-                    IconButton(onClick = {
-                        viewModel.onEvent(TodoEvent.DeleteTodo(todo))
-                    }) {
-                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Todo")
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(18.dp))
+                        .background(Color.DarkGray)
+                        .padding(12.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .clip(CircleShape)
+                                .background(Color.Blue),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = todo.image)
+                        }
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(text = todo.title)
+                            Text(text = todo.text)
+                        }
+                        IconButton(onClick = {
+                            viewModel.onEvent(TodoEvent.DeleteTodo(todo))
+                        }) {
+                            Icon(
+                                imageVector = Icons.TwoTone.Delete,
+                                contentDescription = "Delete Todo",
+                                tint = Color.Red,
+                                modifier = Modifier.size(30.dp)
+                            )
+                        }
                     }
                 }
             }
